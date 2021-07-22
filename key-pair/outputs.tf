de85e09bf12d8ad926437d07897fa7f8635f908d
miyasemh <macymehmed@gmail.com>
2021-07-22 19:58:00 +0000

    output key id applied

--- key-pair/outputs.tf
+++ key-pair/outputs.tf
@@ -2,7 +2,11 @@ output "region" {
   value = var.region
 }
 
-output name {
-  value       = var.key_name
+output "key_name" {
+  value = var.key_name
+}
+
+output "key_id" {
+  value = aws_key_pair.task2.id
 }
 
